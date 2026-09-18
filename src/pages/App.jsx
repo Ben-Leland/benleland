e9554e4e8b69732f572ee064083a47d208668d17
Ben Leland <ben_leland@comcast.net>
2026-09-17 20:30:30 -0700

    Fix reload 404 bug

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,25 +1,22 @@
 import React, { useState, useEffect } from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home.jsx";
 import Navbar from "../components/Navbar/Navbar.jsx";
 import Portfolio from "./Portfolio/Portfolio.jsx";
 import Contact from "./Contact/Contact.jsx";
 import "./App.css";
 
-const basename = process.env.NODE_ENV == "production" ? "/benleland" : "/";
-
-// TODO: find overall theme of website
 export default function App() {
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/portfolio" element={<Portfolio />} />
-                    <Route path="/contact" element={<Contact />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
 }
