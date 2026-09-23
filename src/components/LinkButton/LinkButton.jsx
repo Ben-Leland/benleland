577ef21200b5da14702042a2384714687ca7214a
Ben Leland <ben_leland@comcast.net>
2026-09-22 17:07:23 -0700

    Fix linkbutton icon to show icon on mobile

--- src/components/LinkButton/LinkButton.jsx
+++ src/components/LinkButton/LinkButton.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./LinkButton.css";
+import { ArrowDownToLine, ArrowUpRight } from "lucide-react";
 
 export default function LinkButton({ type, theme, address, text, downloadTitle }) {
     if (type == "link") {
         return (
-            <Link to={address} className={`link-button ${theme}`}>{text}</Link>
+            <Link to={address} className={`link-button ${theme}`}>{text} &nbsp;&nbsp; {<ArrowUpRight size={14}/>}</Link>
         );
     } else if (type == "a") {
         return (
-            <a href={address} className={`link-button ${theme}`} target="_blank" rel="noreferrer">{text}</a>
+            <a href={address} className={`link-button ${theme}`} target="_blank" rel="noreferrer">{text} &nbsp;&nbsp; {<ArrowUpRight size={14}/>}</a>
         );
     } else if (type == "download") {
         return (
-            <a href={address} className={`link-button ${theme}`} target="_blank" rel="noreferrer">{text}</a>
+            <a href={address} className={`link-button ${theme}`} target="_blank" rel="noreferrer">{text} &nbsp;&nbsp; {<ArrowDownToLine size={14} />}</a>
         );
     }
 }
